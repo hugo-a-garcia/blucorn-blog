8a64a7428af69cd0d82fae4468a4d69b4cd6f04e
hugo-a-garcia <hugo.a.garcia@gmail.com>
2021-04-23 07:49:11 +0200

    Basic initial modification

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,32 @@
 module.exports = {
   siteMetadata: {
-    title: "bluecorn-blog",
+    // Used for the site title and SEO
+    title: `BlueCorn`,
+    // Used to provide alt text for your avatar
+    author: `Hugo A. Garcia`,
+    // Used for SEO
+    description: `A Journey into Robotics`,
+    // Used for resolving images in social cards
+    siteUrl: `https://bluecorn.dev`,
+    // Used for social links in the root footer
+    social: [
+      {
+        name: `Twitter`,
+        url: `https://twitter.com/gatsbyjs`,
+      },
+      {
+        name: `GitHub`,
+        url: `https://github.com/bluecorn`,
+      },
+    ],
   },
-  plugins: [],
-};
+  plugins: [
+    {
+      resolve: `gatsby-theme-blog`,
+      options: {
+        basePath: `/blog`,
+        prismPreset: `prism-okaidia`,
+      },
+    },
+  ],
+}
